5ac64a4b00f0f428724d240d2239f4bdec3cedde
LukeGrahamLandry <graham11000@gmail.com>
2022-12-21 05:25:57 -0500

    restructure

---
+++ Fabric/src/main/java/ca/lukegrahamlandry/mercenaries/FabricModMain.java
@@ -0,0 +1,10 @@
+package ca.lukegrahamlandry.mercenaries;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricModMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
