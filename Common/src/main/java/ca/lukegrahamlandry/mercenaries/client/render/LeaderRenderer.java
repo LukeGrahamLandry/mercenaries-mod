d11f7bee843c856ba03fc6a93b06dfdc49e939f2
LukeGrahamLandry <graham11000@gmail.com>
2022-12-22 13:05:40 -0500

    port entity renderers

--- Common/src/main/java/ca/lukegrahamlandry/mercenaries/client/render/LeaderRenderer.java
+++ Common/src/main/java/ca/lukegrahamlandry/mercenaries/client/render/LeaderRenderer.java
@@ -1,47 +1,61 @@
 package ca.lukegrahamlandry.mercenaries.client.render;
 
 import ca.lukegrahamlandry.mercenaries.entity.LeaderEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.layers.*;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.phys.Vec3;
 
 public class LeaderRenderer extends LivingEntityRenderer<LeaderEntity, PlayerModel<LeaderEntity>> {
     public LeaderRenderer(EntityRendererProvider.Context context) {
-        this(context, false);
+        super(context, new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER), false), 0.5F);
+        this.addLayer(new HumanoidArmorLayer<>(this, new HumanoidModel<>(context.bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)), new HumanoidModel<>(context.bakeLayer(ModelLayers.PLAYER_OUTER_ARMOR))));
+        this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()));
+        this.addLayer(new ArrowLayer<>(context, this));
+        this.addLayer(new CustomHeadLayer<>(this, context.getModelSet(), context.getItemInHandRenderer()));
+        this.addLayer(new ElytraLayer<>(this, context.getModelSet()));
     }
 
-    public LeaderRenderer(EntityRendererProvider.Context context, boolean p_i46103_2_) {
-        super(context, new PlayerModel<>(0.0F, p_i46103_2_), 0.5F);
-        this.addLayer(new BipedArmorLayer<>(this, new BipedModel(0.5F), new BipedModel(1.0F)));
-        this.addLayer(new HeldItemLayer<>(this));
-        this.addLayer(new ArrowLayer<>(this));
-        this.addLayer(new HeadLayer<>(this));
-        this.addLayer(new ElytraLayer<>(this));
-    }
-
-    public void render(LeaderEntity p_225623_1_, float p_225623_2_, float p_225623_3_, MatrixStack p_225623_4_, IRenderTypeBuffer p_225623_5_, int p_225623_6_) {
+    @Override
+    public void render(LeaderEntity p_225623_1_, float p_225623_2_, float p_225623_3_, PoseStack p_225623_4_, MultiBufferSource p_225623_5_, int p_225623_6_) {
         this.setModelProperties(p_225623_1_);
         super.render(p_225623_1_, p_225623_2_, p_225623_3_, p_225623_4_, p_225623_5_, p_225623_6_);
     }
 
-    public Vector3d getRenderOffset(LeaderEntity p_225627_1_, float p_225627_2_) {
-        return p_225627_1_.isCrouching() ? new Vector3d(0.0D, -0.125D, 0.0D) : super.getRenderOffset(p_225627_1_, p_225627_2_);
+    @Override
+    public Vec3 getRenderOffset(LeaderEntity p_225627_1_, float p_225627_2_) {
+        return p_225627_1_.isCrouching() ? new Vec3(0.0D, -0.125D, 0.0D) : super.getRenderOffset(p_225627_1_, p_225627_2_);
     }
 
     private void setModelProperties(LeaderEntity p_177137_1_) {
         PlayerModel<LeaderEntity> playermodel = this.getModel();
         playermodel.setAllVisible(true);
         playermodel.crouching = p_177137_1_.isCrouching();
-        BipedModel.ArmPose bipedmodel$armpose = getArmPose(p_177137_1_, Hand.MAIN_HAND);
-        BipedModel.ArmPose bipedmodel$armpose1 = getArmPose(p_177137_1_, Hand.OFF_HAND);
+        HumanoidModel.ArmPose bipedmodel$armpose = getArmPose(p_177137_1_, InteractionHand.MAIN_HAND);
+        HumanoidModel.ArmPose bipedmodel$armpose1 = getArmPose(p_177137_1_, InteractionHand.OFF_HAND);
         if (bipedmodel$armpose.isTwoHanded()) {
-            bipedmodel$armpose1 = p_177137_1_.getOffhandItem().isEmpty() ? BipedModel.ArmPose.EMPTY : BipedModel.ArmPose.ITEM;
+            bipedmodel$armpose1 = p_177137_1_.getOffhandItem().isEmpty() ? HumanoidModel.ArmPose.EMPTY : HumanoidModel.ArmPose.ITEM;
         }
 
-        if (p_177137_1_.getMainArm() == HandSide.RIGHT) {
+        if (p_177137_1_.getMainArm() == HumanoidArm.RIGHT) {
             playermodel.rightArmPose = bipedmodel$armpose;
             playermodel.leftArmPose = bipedmodel$armpose1;
         } else {
@@ -50,33 +64,33 @@ public class LeaderRenderer extends LivingEntityRenderer<LeaderEntity, PlayerMod
         }
     }
 
-    private static BipedModel.ArmPose getArmPose(LeaderEntity p_241741_0_, Hand p_241741_1_) {
+    private static HumanoidModel.ArmPose getArmPose(LeaderEntity p_241741_0_, InteractionHand p_241741_1_) {
         ItemStack itemstack = p_241741_0_.getItemInHand(p_241741_1_);
         if (itemstack.isEmpty()) {
-            return BipedModel.ArmPose.EMPTY;
+            return HumanoidModel.ArmPose.EMPTY;
         } else {
             if (p_241741_0_.getUsedItemHand() == p_241741_1_ && p_241741_0_.getUseItemRemainingTicks() > 0) {
-                UseAction useaction = itemstack.getUseAnimation();
-                if (useaction == UseAction.BLOCK) {
-                    return BipedModel.ArmPose.BLOCK;
+                UseAnim useaction = itemstack.getUseAnimation();
+                if (useaction == UseAnim.BLOCK) {
+                    return HumanoidModel.ArmPose.BLOCK;
                 }
 
-                if (useaction == UseAction.BOW) {
-                    return BipedModel.ArmPose.BOW_AND_ARROW;
+                if (useaction == UseAnim.BOW) {
+                    return HumanoidModel.ArmPose.BOW_AND_ARROW;
                 }
 
-                if (useaction == UseAction.SPEAR) {
-                    return BipedModel.ArmPose.THROW_SPEAR;
+                if (useaction == UseAnim.SPEAR) {
+                    return HumanoidModel.ArmPose.THROW_SPEAR;
                 }
 
-                if (useaction == UseAction.CROSSBOW && p_241741_1_ == p_241741_0_.getUsedItemHand()) {
-                    return BipedModel.ArmPose.CROSSBOW_CHARGE;
+                if (useaction == UseAnim.CROSSBOW && p_241741_1_ == p_241741_0_.getUsedItemHand()) {
+                    return HumanoidModel.ArmPose.CROSSBOW_CHARGE;
                 }
             } else if (!p_241741_0_.swinging && itemstack.getItem() == Items.CROSSBOW && CrossbowItem.isCharged(itemstack)) {
-                return BipedModel.ArmPose.CROSSBOW_HOLD;
+                return HumanoidModel.ArmPose.CROSSBOW_HOLD;
             }
 
-            return BipedModel.ArmPose.ITEM;
+            return HumanoidModel.ArmPose.ITEM;
         }
     }
 
@@ -84,21 +98,21 @@ public class LeaderRenderer extends LivingEntityRenderer<LeaderEntity, PlayerMod
         return merc.getTexture();
     }
 
-    protected void scale(LeaderEntity p_225620_1_, MatrixStack p_225620_2_, float p_225620_3_) {
+    protected void scale(LeaderEntity p_225620_1_, PoseStack p_225620_2_, float p_225620_3_) {
         float f = 0.9375F;
         p_225620_2_.scale(0.9375F, 0.9375F, 0.9375F);
     }
 
 
-    public void renderRightHand(MatrixStack p_229144_1_, IRenderTypeBuffer p_229144_2_, int p_229144_3_, LeaderEntity p_229144_4_) {
+    public void renderRightHand(PoseStack p_229144_1_, MultiBufferSource p_229144_2_, int p_229144_3_, LeaderEntity p_229144_4_) {
         this.renderHand(p_229144_1_, p_229144_2_, p_229144_3_, p_229144_4_, (this.model).rightArm, (this.model).rightSleeve);
     }
 
-    public void renderLeftHand(MatrixStack p_229146_1_, IRenderTypeBuffer p_229146_2_, int p_229146_3_, LeaderEntity p_229146_4_) {
+    public void renderLeftHand(PoseStack p_229146_1_, MultiBufferSource p_229146_2_, int p_229146_3_, LeaderEntity p_229146_4_) {
         this.renderHand(p_229146_1_, p_229146_2_, p_229146_3_, p_229146_4_, (this.model).leftArm, (this.model).leftSleeve);
     }
 
-    private void renderHand(MatrixStack p_229145_1_, IRenderTypeBuffer p_229145_2_, int p_229145_3_, LeaderEntity p_229145_4_, ModelRenderer p_229145_5_, ModelRenderer p_229145_6_) {
+    private void renderHand(PoseStack p_229145_1_, MultiBufferSource p_229145_2_, int p_229145_3_, LeaderEntity p_229145_4_, ModelPart p_229145_5_, ModelPart p_229145_6_) {
         PlayerModel<LeaderEntity> playermodel = this.getModel();
         this.setModelProperties(p_229145_4_);
         playermodel.attackTime = 0.0F;
@@ -111,29 +125,29 @@ public class LeaderRenderer extends LivingEntityRenderer<LeaderEntity, PlayerMod
         p_229145_6_.render(p_229145_1_, p_229145_2_.getBuffer(RenderType.entityTranslucent(p_229145_4_.getTexture())), p_229145_3_, OverlayTexture.NO_OVERLAY);
     }
 
-    protected void setupRotations(LeaderEntity p_225621_1_, MatrixStack p_225621_2_, float p_225621_3_, float p_225621_4_, float p_225621_5_) {
+    protected void setupRotations(LeaderEntity p_225621_1_, PoseStack p_225621_2_, float p_225621_3_, float p_225621_4_, float p_225621_5_) {
         float f = p_225621_1_.getSwimAmount(p_225621_5_);
         if (p_225621_1_.isFallFlying()) {
             super.setupRotations(p_225621_1_, p_225621_2_, p_225621_3_, p_225621_4_, p_225621_5_);
             float f1 = (float)p_225621_1_.getFallFlyingTicks() + p_225621_5_;
-            float f2 = MathHelper.clamp(f1 * f1 / 100.0F, 0.0F, 1.0F);
+            float f2 = Mth.clamp(f1 * f1 / 100.0F, 0.0F, 1.0F);
             if (!p_225621_1_.isAutoSpinAttack()) {
-                p_225621_2_.mulPose(Vector3f.XP.rotationDegrees(f2 * (-90.0F - p_225621_1_.xRot)));
+                p_225621_2_.mulPose(Vector3f.XP.rotationDegrees(f2 * (-90.0F - p_225621_1_.getXRot())));
             }
 
-            Vector3d vector3d = p_225621_1_.getViewVector(p_225621_5_);
-            Vector3d vector3d1 = p_225621_1_.getDeltaMovement();
-            double d0 = Entity.getHorizontalDistanceSqr(vector3d1);
-            double d1 = Entity.getHorizontalDistanceSqr(vector3d);
+            Vec3 v = p_225621_1_.getViewVector(p_225621_5_);
+            Vec3 v1 = p_225621_1_.getDeltaMovement();
+            double d0 = v1.horizontalDistanceSqr();
+            double d1 = v.horizontalDistanceSqr();
             if (d0 > 0.0D && d1 > 0.0D) {
-                double d2 = (vector3d1.x * vector3d.x + vector3d1.z * vector3d.z) / Math.sqrt(d0 * d1);
-                double d3 = vector3d1.x * vector3d.z - vector3d1.z * vector3d.x;
+                double d2 = (v1.x * v.x + v1.z * v.z) / Math.sqrt(d0 * d1);
+                double d3 = v1.x * v.z - v1.z * v.x;
                 p_225621_2_.mulPose(Vector3f.YP.rotation((float)(Math.signum(d3) * Math.acos(d2))));
             }
         } else if (f > 0.0F) {
             super.setupRotations(p_225621_1_, p_225621_2_, p_225621_3_, p_225621_4_, p_225621_5_);
-            float f3 = p_225621_1_.isInWater() ? -90.0F - p_225621_1_.xRot : -90.0F;
-            float f4 = MathHelper.lerp(f, 0.0F, f3);
+            float f3 = p_225621_1_.isInWater() ? -90.0F - p_225621_1_.getXRot() : -90.0F;
+            float f4 = Mth.lerp(f, 0.0F, f3);
             p_225621_2_.mulPose(Vector3f.XP.rotationDegrees(f4));
             if (p_225621_1_.isVisuallySwimming()) {
                 p_225621_2_.translate(0.0D, -1.0D, (double)0.3F);
